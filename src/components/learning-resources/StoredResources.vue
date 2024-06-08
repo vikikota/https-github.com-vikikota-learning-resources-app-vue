2815352284f92269dad8a359b5f0b0d2345b080d
vikikota <viktoria.kota@gmail.com>
2024-06-09 00:18:23 +0200

    add card and button component

---
+++ src/components/learning-resources/StoredResources.vue
@@ -0,0 +1,36 @@
+<template>
+  <ul>
+    <learning-source
+      v-for="resource in resources"
+      :key="resource.id"
+      :title="resource.title"
+      :description="resource.description"
+      :link="resource.link"
+    >
+    </learning-source>
+  </ul>
+</template>
+
+<script>
+import LearningSource from './LearningSource.vue';
+
+export default {
+  components: { LearningSource },
+  props: ["resources"],
+  data() {
+    return {
+
+    }
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  margin: auto;
+  max-width: 40rem;
+}
+</style>
